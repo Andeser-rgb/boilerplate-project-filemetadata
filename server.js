6f0e9af5a8cf3b90dff3083ccee41a775408c0bd
Andeser-rgb <andeser@protonmail.com>
2021-07-26 16:28:36 +0200

    Working file uploading and metadata

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+var multer = require('multer');
+var upload = multer({dest: 'uploads/'});
 require('dotenv').config()
 
 var app = express();
@@ -12,6 +14,16 @@ app.get('/', function (req, res) {
 });
 
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  let oName = req.file.originalname;
+  let type = req.file.mimetype;
+  let size = req.file.size;
+  res.json({
+    name: oName,
+    type: type,
+    size: size
+  });
+});
 
 
 const port = process.env.PORT || 3000;
